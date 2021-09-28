df95e5a29777667aaea0adbe38847de2bebe2c67
jonahvimahi <jonahvimahi@gmail.com>
2021-09-28 14:14:02 -0600

    added reverse words

--- Codewars1.js
+++ Codewars1.js
@@ -33,3 +33,14 @@ function lovefunc(flower1, flower2){
       return true
     }
   }
+
+  //Reverse Words
+  function reverseWords(str) {
+    let newStr = str.split(" ")
+    finalArr = []
+  
+    for(let i = 0; i < newStr.length; i++){
+      finalArr.push(newStr[i].split("").reverse().join(""))
+    }
+    return finalArr.join(' ')
+  }
